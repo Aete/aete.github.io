2eaaf2141b916bf5b40a692bd7dee6681c59a73a
Aete <hsg8234@naver.com>
2017-11-03 22:55:27 -0400

    update at 11.03
    design front page

---
+++ draft/js/activate.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#home").hover(function(){
+        $(this).css("color", "dodgerblue");
+    }, function(){
+        $(this).css("color", "black");
+    });
+    $("#design").hover(function(){
+        $(this).css("color", "crimson");
+    }, function(){
+        $(this).css("color", "black");
+    });
+    $("#data").hover(function(){
+        $(this).css("color", "MediumSeaGreen");
+    }, function(){
+        $(this).css("color", "black");
+    });
+    $("#paper").hover(function(){
+        $(this).css("color", "purple");
+    }, function(){
+        $(this).css("color", "black");
+    });
+    $("#about").hover(function(){
+        $(this).css("color", "dodgerblue");
+    }, function(){
+        $(this).css("color", "black");
+    });
+});
