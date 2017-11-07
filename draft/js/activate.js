f327834d8365b36093711d0d48682d2aeea42cd9
Aete <hsg8234@naver.com>
2017-11-06 19:37:05 -0500

    make some animation

--- draft/js/activate.js
+++ draft/js/activate.js
@@ -1,38 +1,45 @@
 $(document).ready(function(){
     $("#home").hover(function(){
-        $(this).css("color", "crimson");
+        $(this).css("color", "#00BCD4");
     }, function(){
         $(this).css("color", "black");
     });
+
     $("#design").hover(function(){
-        $(this).css("color", "crimson");
+        $(this).css("border-top", "3px solid #F44336");
+        $(this).css("font-weight", "700");
     }, function(){
-        $(this).css("color", "black");
+        $(this).css("border-top", "3px solid white");
+        $(this).css("font-weight", "400");
     });
     $("#data").hover(function(){
-        $(this).css("color", "crimson");
+        $(this).css("border-top", "3px solid #4CAF50");
+        $(this).css("font-weight", "700");
     }, function(){
-        $(this).css("color", "black");
+        $(this).css("border-top", "3px solid white");
+        $(this).css("font-weight", "400");
     });
     $("#paper").hover(function(){
-        $(this).css("color", "crimson");
+        $(this).css("border-top", "3px solid #9C27B0");
+        $(this).css("font-weight", "700");
     }, function(){
-        $(this).css("color", "black");
+        $(this).css("border-top", "3px solid white");
+        $(this).css("font-weight", "400");
     });
     $("#about").hover(function(){
-        $(this).css("color", "crimson");
+        $(this).css("border-top", "3px solid #00BCD4");
+        $(this).css("font-weight", "700");
     }, function(){
-        $(this).css("color", "black");
+        $(this).css("border-top", "3px solid white");
+        $(this).css("font-weight", "400");
     });
     $("#nav_button").hover(function(){
-        $(this).css("color", "crimson");
-        $(this).css("cursor", "pointer");
+        $(this).css("color", "#00BCD4");
     }, function(){
         $(this).css("color", "black");
     });
     $(".button").hover(function(){
         $(this).css("color", "dimgray");
-        $(this).css("cursor", "pointer");
     }, function(){
         $(this).css("color", "black");
     });
