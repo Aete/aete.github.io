7e9e79634c3a00c527f9babf6a45993b13900ee1
Aete <hsg8234@naver.com>
2017-11-10 04:43:30 -0500

    write resume

--- draft/js/activate.js
+++ draft/js/activate.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-    $("#home").hover(function(){
+$(document).ready(function() {
+    /* action with mouse hovering*/
+    $("#home").hover(function () {
         $(this).css("color", "#00BCD4");
-    }, function(){
+    }, function () {
         $(this).css("color", "black");
     });
-
-    $("#design").hover(function(){
+    $("#design").hover(function () {
         $(this).css("border-top", "3px solid #F44336");
         $(this).css("font-weight", "700");
-    }, function(){
+    }, function () {
         $(this).css("border-top", "3px solid white");
         $(this).css("font-weight", "400");
     });
-    $("#data").hover(function(){
+    $("#data").hover(function () {
         $(this).css("border-top", "3px solid #4CAF50");
         $(this).css("font-weight", "700");
-    }, function(){
+    }, function () {
         $(this).css("border-top", "3px solid white");
         $(this).css("font-weight", "400");
     });
-    $("#paper").hover(function(){
+    $("#paper").hover(function () {
         $(this).css("border-top", "3px solid #9C27B0");
         $(this).css("font-weight", "700");
-    }, function(){
+    }, function () {
         $(this).css("border-top", "3px solid white");
         $(this).css("font-weight", "400");
     });
-    $("#about").hover(function(){
+    $("#about").hover(function () {
         $(this).css("border-top", "3px solid #00BCD4");
         $(this).css("font-weight", "700");
-    }, function(){
+    }, function () {
         $(this).css("border-top", "3px solid white");
         $(this).css("font-weight", "400");
     });
-    $("#nav_button").hover(function(){
+    $("#nav_button").hover(function () {
         $(this).css("color", "#00BCD4");
-    }, function(){
+    }, function () {
         $(this).css("color", "black");
     });
-    $(".button").hover(function(){
+    $(".button").hover(function () {
         $(this).css("color", "dimgray");
-    }, function(){
+    }, function () {
         $(this).css("color", "black");
     });
-});
+}
