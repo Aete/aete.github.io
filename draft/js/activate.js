6385b972ca8c58fd7e6ee402e27d7590b0dd12c1
Aete <hsg8234@naver.com>
2017-11-30 07:40:32 -0500

    front page layout finish

--- draft/js/activate.js
+++ draft/js/activate.js
@@ -1,10 +1,7 @@
-
-
-
 /* This is related to header part*/
 var projectColor = "#EF5350";
 var ideaColor = "#4CAF50";
-var aboutColor = "royalblue";
+var profileColor = "royalblue";
 
 $(".nav_content").hover(function () {
     if($(this).attr('id')==='project'){
@@ -14,7 +11,7 @@ $(".nav_content").hover(function () {
         $(this).css("color", ideaColor);
     }
     else{
-        $(this).css("color", aboutColor);
+        $(this).css("color", profileColor);
     }
 }, function () {
     $(this).css("color", "black");
@@ -29,6 +26,58 @@ $("#nav_menu").first().click(function () {
     $("#nav_menu").css("left", "800px");
     $("#nav_menu a").css("animation","moveback 0.7s");
 });
+
+
+/* this is related to square selection*/
+var category1 = 0 ;
+
+$(".nav_content").click(function(){
+    if($(this).attr('id')==='project'){
+        category1 = "project";
+    }
+    else if($(this).attr('id')==='idea'){
+        category1= "idea";
+    }
+    else{
+        category1 = "profile";
+    }
+    squareSelector();
+});
+
+$("#category").change(function(){
+    if($("#category>option:selected").attr("value") ==="project"){
+        category1 = "project";
+    }
+    else if($("#category>option:selected").attr("value") ==="idea"){
+        category1= "idea";
+    }
+    else if($("#category>option:selected").attr("value") ==='profile'){
+        category1 = "profile"
+    }
+    else{
+        category1 = "all"
+    }
+    squareSelector();
+});
+
+var squareSelector = function(){
+    if(category1==="project"){
+        $(".square").css("display","none");
+        $(".project").css("display","flex");
+    }
+    else if(category1==="idea"){
+        $(".square").css("display","none");
+        $(".idea").css("display","flex");
+    }
+    else if(category1 === "profile"){
+        $(".square").css("display","none");
+        $(".profile").css("display","flex");
+    }
+    else{
+        $(".square").css("display","flex");
+    }
+
+};
 
 
 /* this is related to drop-down menu*/
