f770b4c61a16f6f9648180089b81ac6b8853e4ac
Aete <hsg8234@naver.com>
2017-11-30 19:06:34 -0500

    Finish Dropdown and square align

--- draft/js/activate.js
+++ draft/js/activate.js
@@ -20,11 +20,26 @@ $(".nav_content").hover(function () {
 
 /* this is related to navigation bar*/
 $("#nav_button").click(function () {
-    $("#nav_menu").css("left", "calc(100vw - 230px)");
+    $(".nav_menu").css("left", "calc(100vw - 230px)");
 });
-$("#nav_menu").first().click(function () {
-    $("#nav_menu").css("left", "800px");
-    $("#nav_menu a").css("animation","moveback 0.7s");
+$(".nav_menu").first().click(function () {
+    $(".nav_menu").css("left", "800px");
+    $(".nav_menu a").css("animation","moveback 0.7s");
+});
+
+$(".nav_menu_box").click(function () {
+    if($(this).attr('id')==='project_nav'){
+        category1 ="project";
+    }
+    else if($(this).attr('id')==='idea_nav'){
+        category1= "idea";
+    }
+    else{
+        category1 = "profile";
+    }
+    squareSelector();
+    $(".nav_menu").css("left", "800px");
+    $(".nav_menu a").css("animation","moveback 0.7s");
 });
 
 
@@ -41,6 +56,11 @@ $(".nav_content").click(function(){
     else{
         category1 = "profile";
     }
+    squareSelector();
+});
+
+$("#home").click(function(){
+    category1="all";
     squareSelector();
 });
 
@@ -64,23 +84,60 @@ var squareSelector = function(){
     if(category1==="project"){
         $(".square").css("display","none");
         $(".project").css("display","flex");
+        $("#category").html("<option value='project'>Project</option>" +
+            "<option value = 'all'>All</option>" +
+            "<option value='idea'>Idea</option>" +
+            "<option value='profile'>Profile</option>");
     }
     else if(category1==="idea"){
         $(".square").css("display","none");
         $(".idea").css("display","flex");
+        $("#category").html("<option value='idea'>Idea</option>" +
+            "<option value = 'all'>All</option>" +
+            "<option value='project'>Project</option>" +
+            "<option value='profile'>Profile</option>");
     }
     else if(category1 === "profile"){
         $(".square").css("display","none");
         $(".profile").css("display","flex");
+        $("#category").html("<option value='profile'>Profile</option>" +
+            "<option value = 'all'>All</option>" +
+            "<option value='project'>Project</option>" +
+            "<option value='idea'>Idea</option>");
     }
-    else{
+    else if(category1==="all"){
         $(".square").css("display","flex");
-    }
-
+        $("#category").html(
+            "<option value = 'all'>All</option>" +
+            "<option value='project'>Project</option>" +
+            "<option value='idea'>Idea</option>"+
+            "<option value='profile'>Profile</option>");
+}
+    subSelector();
 };
 
 
-/* this is related to drop-down menu*/
+var subSelector = function(){
+    if(category1==="project") {
+        $("#category_sub").html("<option value = 'all'>All</option>" +
+            "<option value = 'competition'>Competition</option>" +
+            "<option value = 'others'>Others</option>");
+    }
+    else if(category1 === "idea") {
+        $("#category_sub").html("<option value = 'all'>All</option>" +
+            "<option value = 'essay'>Essay</option>" +
+            "<option value = 'papers'>Paper</option>" +
+            "<option value = 'timeline'>Timeline</option>");
+    }
+    else{
+        $("#category_sub").html("<option>Sub-Category</option>")
+    }
+};
+
+
+
+
+
 
 
 
