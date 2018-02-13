4526c8973fd1516a91efd79f44ed1cd277f67250
Aete <hsg8234@naver.com>
2018-02-13 19:01:26 +0900

    add images

--- js/model.js
+++ js/model.js
@@ -9,6 +9,7 @@ var projectList = [
     {"title":"The Frame", "time" : 2017, "category" : "project"},
     {"title":"Profile", "time" : null, "category" : "profile"},
     {"title":"QR-Driven City", "time" : 2013, "category" : "idea"},
+    {"title":"Collaboration", "time" : 2015, "category" : "Project"},
     {"title":"Writing", "time" : null, "category" : "idea"},
     {"title":"Pengunticon", "time" : 2013, "category" : "project"},
     {"title":"Dwelling by Walling", "time" : 2017, "category" : "project"},
@@ -21,6 +22,16 @@ for (var i = 0; i<projectList.length;i++){
     if(projectList[i].title==='QR-Driven City'){
         projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".gif' class='logo' style='width:90%;'>";
     }
+    else if(projectList[i].title==='Dwelling by Walling'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:95%;'>";
+    }
+    else if(projectList[i].title==='XS, S, M, L, XL'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:95%;'>";
+    }
+    else if(projectList[i].title==='Collaboration'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:75%;'>";
+    }
+
 }
 
 
