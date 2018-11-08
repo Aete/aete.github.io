82b1359764c2b906d74ba22be9ede2618bb088b1
Aete <hsg8234@naver.com>
2018-11-08 10:21:02 +0900

    start to describe qr city project

--- js/model.js
+++ js/model.js
@@ -3,7 +3,7 @@ var projectList = [
     {"title":"Plan Generator using DCGANs", "time" : 2018, "category" : "idea"},
     {"title":"Timeline of Neighborhood", "time" : 2018, "category" : "idea"},
     {"title":"The Frame", "time" : 2017, "category" : "project"},
-    {"title":"QR-Driven City", "time" : 2013, "category" : "idea"},
+    {"title":"QR City", "time" : 2013, "category" : "idea"},
    // {"title":"Collaboration", "time" : 2015, "category" : "Project"},
    // {"title":"Writing", "time" : null, "category" : "idea"},
     {"title":"Dwelling by Walling", "time" : 2016, "category" : "project"},
@@ -16,7 +16,7 @@ var projectList = [
 var info_list=[
     {"title":"Maps",  "script" : null, "person" : "Han Seung Gyun"},
     {"title":"The Frame", "script" : "Graduation work in SKKU", "person" : "Design excellence award (1st prize) in SKKU spring semester, 2017"},
-    {"title":"QR-Driven City", "script" : "Final Project at SKKU Design Modeling Class(Spring Semester, 2013)", "person" : "Han Seung Gyun, Jung Tae Jung, Jae Jun Hyung and Bang Jun Gyu(Department of Architecture, SKKU)"},
+    {"title":"QR City", "script" : "Final Project at SKKU Design Modeling Class(Spring Semester, 2013)", "person" : "Han Seung Gyun, Jung Tae Jung, Jae Jun Hyung and Bang Jun Gyu(Department of Architecture, SKKU)"},
     //{"title":"Collaboration", "script" : "10th East Asia Urban Architecture Joint Studio 2015", "person" : "Han Seung Gyun, Lee Dong Yeong, Akira Ooka, Yuna Kinoshita"},
     //{"title":"Writing", "script" : null, "person" : null},
     {"title":"Dwelling by Walling", "script" : "Bronze Prize in Gyeonggi Architecture Fair", "person" : "Han Seung Gyun, Choi Tae Won, Kim Ji Soo"},
@@ -25,7 +25,7 @@ var info_list=[
 
 for (var i = 0; i<projectList.length;i++){
     projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".svg' class='logo'>";
-    if(projectList[i].title==='QR-Driven City'){
+    if(projectList[i].title==='QR City'){
         projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".gif' class='logo' style='width:90%;'>";
     }
     else if(projectList[i].title==='Dwelling by Walling'){
