8f2de74e9b1f3de5e8e360d150be0672ea09efc8
Aete <hsg8234@naver.com>
2018-11-15 23:55:28 +0900

    change the logo, title and add some squares

--- js/app_square.js
+++ js/app_square.js
@@ -8,7 +8,13 @@ var square_making = function(list){
         $(".sub_container:last-child").addClass(list[i].category);
         $(".sub_container:last-child").attr('id',list[i].title);
         console.log(list[i].title);
+        if(list[i].title === "Github"){
+            console.log(1);
+            $(".sub_container:last-child").attr('onclick','location.href= "https://github.com/aete"');
+        }
+        else{
         $(".sub_container:last-child").attr('onclick','location.href= '+"'"+list[i].title+".html';");
+        }
         $(".sub_container:last-child").append(square);
         $(".square:last-child").append(list[i].imgsrc );
         if(list[i].time != null){
--- js/model.js
+++ js/model.js
@@ -1,15 +1,13 @@
 var projectList = [
-    {"title":"Maps", "time" : 2018, "category" : "idea"},
-    {"title":"Plan Generator <br> using deep learning", "time" : 2018, "category" : "idea"},
-    //{"title":"Timeline of Neighborhood", "time" : 2018, "category" : "idea"},
+    {"title":"Plan Generator", "time" : 2018, "category" : "idea"},
     {"title":"The Frame", "time" : 2017, "category" : "project"},
-    {"title":"QR City", "time" : 2013, "category" : "idea"},
-   // {"title":"Collaboration", "time" : 2015, "category" : "Project"},
-   // {"title":"Writing", "time" : null, "category" : "idea"},
     {"title":"Dwelling by Walling", "time" : 2016, "category" : "project"},
     {"title":"XS, S, M, L, XL", "time" : 2015, "category" : "project"},
-    {"title":"CV", "time" : null, "category" : "profile"},
-    {"title":"Links", "time" : null, "category" : "profile"}
+    {"title":"Collaboration", "time" : 2015, "category" : "project"},
+    {"title":"QR City", "time" : 2013, "category" : "idea"},
+    {"title":"Github", "time" : null, "category" : "profile"},
+    {"title":"Maps", "time" : null, "category" : "idea"},
+    {"title":"Profile", "time" : null, "category" : "profile"}
     ];
 
 
@@ -17,14 +15,13 @@ var info_list=[
     {"title":"Maps",  "script" : null, "person" : "Han Seung Gyun"},
     {"title":"The Frame", "script" : "Graduation work in SKKU", "person" : "Design excellence award (1st prize) in SKKU spring semester, 2017"},
     {"title":"QR City", "script" : "Final Project at SKKU Design Modeling Class(Spring Semester, 2013)", "person" : "Han Seung Gyun, Jung Tae Jung, Jae Jun Hyung and Bang Jun Gyu(Department of Architecture, SKKU)"},
-    //{"title":"Collaboration", "script" : "10th East Asia Urban Architecture Joint Studio 2015", "person" : "Han Seung Gyun, Lee Dong Yeong, Akira Ooka, Yuna Kinoshita"},
+    {"title":"Collaboration", "script" : "10th East Asia Urban Architecture Joint Studio 2015", "person" : "Han Seung Gyun, Lee Dong Yeong, Akira Ooka, Yuna Kinoshita"},
     //{"title":"Writing", "script" : null, "person" : null},
     {"title":"Dwelling by Walling", "script" : "Bronze Prize in Gyeonggi Architecture Fair", "person" : "Han Seung Gyun, Choi Tae Won, Kim Ji Soo"},
     {"title":"XS, S, M, L, XL", "script" : "SKKU Design Studio(Fall Semester, 2015)", "person" : "Han Seung Gyun"}
 ];
 
 for (var i = 0; i<projectList.length;i++){
-    projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".svg' class='logo'>";
     if(projectList[i].title==='QR City'){
         projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".gif' class='logo' style='width:90%;'>";
     }
@@ -35,10 +32,19 @@ for (var i = 0; i<projectList.length;i++){
         projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:95%;'>";
     }
     else if(projectList[i].title==='Collaboration'){
-        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:75%;'>";
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:95%;'>";
     }
     else if(projectList[i].title==='Maps'){
         projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".png' class='logo' style='width:95%;'>";
+    }
+    else if(projectList[i].title==='Profile'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".jpg' class='logo' style='width:95%;'>";
+    }
+    else if(projectList[i].title==='The Frame'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".jpg' class='logo' style='width:95%;'>";
+    }
+    else if(projectList[i].title==='Github'){
+        projectList[i].imgsrc = "<image src='image/"+projectList[i].title+".jpg' class='logo' style='width:60%;'>";
     }
 }
 
