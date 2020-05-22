a3048a3d4b5d6ecd19d41a019c54b3d33e765aeb
Seunggyun Han <hsg8234@naver.com>
2020-05-22 13:08:03 -0400

    update sstreet lion result

--- sstreet_lion/map.js
+++ sstreet_lion/map.js
@@ -16,10 +16,9 @@ let map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl(), 'top-left');
 
 // set variables for file addresses
-const urlmatched = 'data/sstreet_matched.geojson';
-const urlunmatched = 'data/sstreet_unmatched.geojson';
-const urlnode = 'data/sstreet_intersection.geojson';
-const urllionnode = 'data/lion_node_man.geojson';
+const urlmatched = 'data/matched_lion.geojson';
+const urlunmatched = 'data/unmatched_lion.geojson';
+const urlSstreet = 'data/ssnetwork.geojson';
 
 map.on('load', function () {
 
@@ -27,16 +26,13 @@ map.on('load', function () {
     window.setInterval(function() {
         map.getSource('matched').setData(urlmatched);
         map.getSource('unmatched').setData(urlunmatched);
-        map.getSource('node').setData(urlnode);
-        map.getSource('node').setData(urllionnode);
-
+        map.getSource('sstreet').setData(urlSstreet);
     }, 2000);
 
     // add sources to the Map
     map.addSource('matched', { type: 'geojson', data:urlmatched, 'generateId': false});
     map.addSource('unmatched', { type: 'geojson', data:urlunmatched, 'generateId': false});
-    map.addSource('node', { type: 'geojson', data:urlnode, 'generateId': false});
-    map.addSource('lionnode', { type: 'geojson', data:urllionnode, 'generateId': false});
+    map.addSource('sstreet', { type: 'geojson', data:urlunmatched, 'generateId': false});
     // add a layer and set properties
 
     map.addLayer({
@@ -50,7 +46,7 @@ map.on('load', function () {
         "paint": {
             "line-color": "#2196F4",
             "line-width": 2,
-            'line-opacity': 0.75
+
         }
     });
 
@@ -65,42 +61,8 @@ map.on('load', function () {
         "paint": {
             "line-color": "#F44336",
             "line-width": 2,
-            'line-opacity': 0.75
+
         }
     });
 
-    map.addLayer({
-        "id": "node",
-        "source": "node",
-        "type": "circle",
-        "paint": {
-            "circle-color": "#2196F3",
-        }
-    });
-
-
-
-    map.on('click', 'matched', function(e) {
-        const coordinates = e.features[0].geometry.coordinates.slice();
-        const shstGeoid = e.features[0].properties['shstGeometryId'];
-        const shstRefid = e.features[0].properties['shstReferenceId'];
-        const lionSegmentid = e.features[0].properties['pp_segmentid'];
-        const text = `<p><b>SharedStreet Geo ID</b>: ${shstGeoid}<br><b>SharedStreet Ref ID</b>: ${shstRefid}<br> <b>LION segment id</b>: ${lionSegmentid}</p>`;
-        const lat =  (coordinates[0][0] + coordinates[1][0])/2;
-        const lng =   (coordinates[0][1] + coordinates[1][1])/2;
-        new mapboxgl.Popup()
-            .setLngLat([lat,lng])
-            .setHTML(text)
-            .addTo(map);
-    });
-
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'matched', function() {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-// Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'matched', function() {
-        map.getCanvas().style.cursor = '';
-    });
 });
