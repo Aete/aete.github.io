23e39df87e2b4534c053d89e090f05ca9638d9e6
Seunggyun Han <hsg8234@naver.com>
2020-10-22 09:05:16 +0900

    update a capstone project, and remove unnecessary files

--- js/box.js
+++ js/box.js
@@ -4,44 +4,51 @@
  * Created by Han on 2018-02-10.
  */
 
-const square_making = function(list){
-    for(let i=0;i<list.length;i++){
-        $("#main_body").append(sub_container);
-        $(".sub_container:last-child").addClass(list[i].category);
-        $(".sub_container:last-child").attr('id',list[i].title);
-        if(list[i].title === "Github"){
-            $(".sub_container:last-child").attr('onclick', "window.open('https://github.com/aete','_blank')");
-        }
-        else if(list[i].title === "Medium"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('https://medium.com/@seunggyunhan','_blank')");}
-
-        else if(list[i].title === "Make it Green<br>Dashboard"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('http://www.han.codes/jc_datadashboard_prototype/','_blank')");}
-
-        else if(list[i].title === "Seoul Living-Pop<br>Dashboard"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('http://www.han.codes/Data-in-Seoul/livingpop/','_blank')");}
-
-        else if(list[i].title === "Linkedin"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('https://www.linkedin.com/in/seunggyunhancodes/','_blank')");}
-
-        else if(list[i].title === "KakaoTalk-Clone"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('https://www.han.codes/Kakaotalk-clone/','_blank')");}
-
-        else if(list[i].title === "Momentum-Clone"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('https://www.han.codes/Momentum_clone/','_blank')");}
-
-        else if(list[i].title === "Monitoring COVID-19 in <br> US Metropolitan Areas"){$(".sub_container:last-child")
-            .attr('onclick', "window.open('https://nyumarron.github.io/covid_msa/','_blank')");}
-
-        else{$(".sub_container:last-child")
-            .attr('onclick','location.href= '+"'"+list[i].title+".html';");}
-
-        $(".sub_container:last-child").append(square);
-        $(".square:last-child").append(list[i].imgsrc );
-        if(list[i].time != null){$(".sub_container:last-child").append(title.replace('%data',list[i].title+'<br>('+list[i].time+')'));}
-        else{$(".sub_container:last-child").append(title.replace('%data',list[i].title));}
+const square_making = function (list) {
+  for (let i = 0; i < list.length; i++) {
+    $('#main_body').append(sub_container);
+    $('.sub_container:last-child').addClass(list[i].category);
+    $('.sub_container:last-child').attr('id', list[i].title);
+    if (list[i].title === 'Github') {
+      $('.sub_container:last-child').attr('onclick', 'window.open(\'https://github.com/aete\',\'_blank\')');
+    } else if (list[i].title === 'Medium') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://medium.com/@seunggyunhan\',\'_blank\')');
+    } else if (list[i].title === 'Make it Green<br>Dashboard') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'http://www.han.codes/jc_datadashboard_prototype/\',\'_blank\')');
+    } else if (list[i].title === 'Seoul Living-Pop<br>Dashboard') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'http://www.han.codes/Data-in-Seoul/livingpop/\',\'_blank\')');
+    } else if (list[i].title === 'Linkedin') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://www.linkedin.com/in/seunggyunhancodes/\',\'_blank\')');
+    } else if (list[i].title === 'KakaoTalk-Clone') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://www.han.codes/Kakaotalk-clone/\',\'_blank\')');
+    } else if (list[i].title === 'Momentum-Clone') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://www.han.codes/Momentum_clone/\',\'_blank\')');
+    } else if (list[i].title === 'Monitoring COVID-19 in <br> US Metropolitan Areas') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://nyumarron.github.io/covid_msa/\',\'_blank\')');
+    } else if (list[i].title === 'Interagency work zone<br>traffic data modeling and analysis') {
+      $('.sub_container:last-child')
+        .attr('onclick', 'window.open(\'https://workzone-collision-analysis.github.io/\',\'_blank\')');
+    } else {
+      $('.sub_container:last-child')
+        .attr('onclick', 'location.href= ' + '\'' + list[i].title + '.html\';');
     }
-    $("#main_body").append(placeholder);
+
+    $('.sub_container:last-child').append(square);
+    $('.square:last-child').append(list[i].imgsrc);
+    if (list[i].time != null) {
+      $('.sub_container:last-child').append(title.replace('%data', list[i].title + '<br>(' + list[i].time + ')'));
+    } else {
+      $('.sub_container:last-child').append(title.replace('%data', list[i].title));
+    }
+  }
+  $('#main_body').append(placeholder);
 };
 
 square_making(projectList);
--- js/model.js
+++ js/model.js
@@ -1,6 +1,7 @@
 let projectList = [
+    {"title": "Interagency work zone<br>traffic data modeling and analysis", "time": "2020", "category": "project"},
     {"title": "Monitoring COVID-19 in <br> US Metropolitan Areas", "time": "~ Present", "category": "project"},
-    {"title": "Seoul Living-Pop<br>Dashboard", "time": "~ Present", "category": "project"},
+    {"title": "Seoul Living-Pop<br>Dashboard", "time": "2020", "category": "project"},
     {"title": "Make it Green<br>Dashboard", "time": 2019, "category": "project"},
     {"title": "Plan Generator", "time": 2018, "category": "idea"},
     {"title": "Dwelling by Walling", "time": 2016, "category": "project"},
@@ -39,19 +40,19 @@ for (let i = 0; i < projectList.length; i++) {
     } else if (projectList[i].title === 'Snowflake') {
         projectList[i].imgsrc = "<image src='image/snowflake.png' class='logo' style='width:60%;'>";
     } else if (projectList[i].title === "Make it Green<br>Dashboard") {
-        console.log('test');
         projectList[i].imgsrc = "<image id = 'jcdashboard' src='image/jc_dashboard.jpg' class='logo'>";
     } else if (projectList[i].title === 'Medium') {
-        projectList[i].imgsrc = "<image id = 'medium' src='image/medium.png' class='logo'>";
+        projectList[i].imgsrc = "<image id = 'medium' src='image/medium.png' class='logo' >";
     } else if (projectList[i].title === "Seoul Living-Pop<br>Dashboard") {
-        projectList[i].imgsrc = "<image id = 'living' src='image/livingpop_dash.png' class='logo'>";
+        projectList[i].imgsrc = "<image id = 'living' src='image/livingpop_dash.png' class='logo' style='width:90%;'>";
     } else if (projectList[i].title === 'KakaoTalk-Clone') {
         projectList[i].imgsrc = "<image id = 'kakao' src='image/kakao.png' class='logo' style='width:70%;'>";
     } else if (projectList[i].title === 'Momentum-Clone') {
         projectList[i].imgsrc = "<image id = 'momentum' src='image/momentum.png' class='logo'>";
     } else if (projectList[i].title === "Monitoring COVID-19 in <br> US Metropolitan Areas") {
-        console.log('test');
-        projectList[i].imgsrc = "<image id = 'COVID' src='image/covid.jpg' class='logo'>";
+        projectList[i].imgsrc = "<image id = 'COVID' src='image/covid.jpg' class='logo' style='width:90%;'>";
+    } else if (projectList[i].title === "Interagency work zone<br>traffic data modeling and analysis") {
+        projectList[i].imgsrc = "<image id = 'workzone' src='image/workzone.png' class='logo' style='width:95%;'>";
     }
 }
 
