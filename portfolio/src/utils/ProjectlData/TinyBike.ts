fb2800aef193f6f9bed03c4f15dde9698d8d2bf9
Seunggyun <hsg8234@naver.com>
2025-02-20 00:04:53 +0900

    adjust tinybike modal animation

--- portfolio/src/utils/ProjectlData/TinyBike.ts
+++ portfolio/src/utils/ProjectlData/TinyBike.ts
@@ -4,7 +4,7 @@ export const TinyBike = {
   awarded:
     "Team Project for Ubiquitous Computing - CS-7470-O01 (Fall 2024 class)",
   description: `
-  <img src="image/tinybikedemotwo.gif" style="width: 300px;" />      
+  <img src="image/tinybikedemotwo.gif" style="max-width: 300px;" />      
   <p> TinyBike is a re-interpretation game project of Nintendo's ExciteBike for Thumby. <br/>
         The game was developed using MicroPython and the Thumby API.  <p/>
         <a href="https://www.youtube.com/shorts/H4ftANam198" target="_blank">Demo Video Link</a>
